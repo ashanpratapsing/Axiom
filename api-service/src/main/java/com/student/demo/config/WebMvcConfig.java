44e7b115a1078f0bf957202f69fd3600101b5d84
ashanpratapsing <ashanpratapsingh05@gmail.com>
2026-06-05 13:06:18 +0530

    Disable WebMvc rate limiter in production

--- api-service/src/main/java/com/student/demo/config/WebMvcConfig.java
+++ api-service/src/main/java/com/student/demo/config/WebMvcConfig.java
@@ -1,11 +1,13 @@
 package com.student.demo.config;
 
+import org.springframework.context.annotation.Profile;
 import com.student.demo.security.RateLimitInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Profile("local")
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final RateLimitInterceptor rateLimitInterceptor;
